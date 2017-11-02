63b58de5c3b06bb27bf6ef8fb52f144cb37d6225
Hans Tercek <hanstercek@wr-130-64-218-39.medford.tufts.edu>
2017-11-02 19:55:33 -0400

    Fig parallax

--- js/script_hans.js
+++ js/script_hans.js
@@ -1,18 +1,51 @@
 jQuery(document).ready(function($) {
-    // $(window).bind('scroll',function(e){
-    // parallaxScroll();
+    // Put all variables in a $(window).resize(function() {
+
+
     // });
 
-    // function parallaxScroll(){
-    //     var scrolled = $(window).scrollTop();
-    //     $('#parallax-bg1').css('top',(0-(scrolled*.25))+'px');
-    //     $('#parallax-bg2').css('top',(0-(scrolled*.5))+'px');
-    //     $('#parallax-bg3').css('top',(0-(scrolled*.75))+'px');
-    // }
-    $(window).scroll(function() {
-        var pageH = $('#page').height() - $(this).height();
-        var pageT = this.scrollY - $('#page').offset().top;
-        
-        $('#block').scrollTop(pageT / pageH * ($('#blockLength').height() - $(this).height()));
+    // Figs Parallax layout
+    var fig_top = ($('#figs').offset()).top;
+    //console.log(fig_top);
+
+    var fig_txt_height = $('#fig-text').height();
+    var figs_height = fig_txt_height * 2.1;
+    $('#figs').css('height', figs_height);
+
+    $('.fig-image').css('top', fig_top);
+    //console.log(figs_height/3);
+    var fig1_top = fig_top + (figs_height/6);
+    var fig2_top = fig_top + (figs_height/2.5);
+    var fig3_top = fig_top + (figs_height - (figs_height/4));
+
+    $('#fig-1').css('top', fig1_top);
+    $('#fig-2').css('top', fig2_top);
+    $('#fig-3').css('top', fig3_top);
+
+    var squig1_top = fig_top + (figs_height/7);
+    var squig2_top = fig_top + (figs_height/2);
+    var squig3_top = fig_top + (figs_height/1.5);
+
+    $('#squiggle-1').css('top', squig1_top);
+    $('#squiggle-2').css('top', squig2_top);
+    $('#squiggle-3').css('top', squig3_top);
+
+    //console.log(fig1_top);
+
+    $(window).bind('scroll',function(e){
+        parallaxScroll();
+        redrawDotNav();
     });
+    function parallaxScroll() {
+        var scrolled = ($(window).scrollTop() - fig_top);
+        if (scrolled <= 0) {scrolled = 0};
+        if (scrolled > (figs_height - fig_txt_height)) {scrolled = (figs_height - fig_txt_height)};
+        $('#fig-text').css('top', (scrolled * .75) + 'px'); // Need to come up with test for this
+        $('#fig-1').css('top', (fig1_top - (scrolled * .25)) + 'px');
+        $('#fig-2').css('top', (fig2_top - (scrolled * .25)) + 'px');
+        $('#fig-3').css('top', (fig3_top - (scrolled * .25)) + 'px');
+        $('#squiggle-1').css('top', squig1_top + 'px');
+        $('#squiggle-2').css('top', squig2_top + 'px');
+        $('#squiggle-3').css('top', squig3_top + 'px');
+    }
 })
