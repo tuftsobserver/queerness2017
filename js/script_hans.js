c3634ccc354196a7f63d387150e31775889f7fa1
Hans Tercek <hanstercek@wr-130-64-218-39.medford.tufts.edu>
2017-11-04 15:51:29 -0400

    Ledditor

--- js/script_hans.js
+++ js/script_hans.js
@@ -5,12 +5,16 @@ jQuery(document).ready(function($) {
     // });
 
 // Potentially create window resize event that retriggers top positioning
+
+    // Ledditor layout
+    $('#ledditor').css('height', $('#letter').height() * 1.25);
+
     // Figs Parallax layout
     var fig_top = ($('#figs').offset()).top;
     //console.log(fig_top);
 
     var fig_txt_height = $('#fig-text').height();
-    var figs_height = fig_txt_height * 2.2;
+    var figs_height = fig_txt_height * 2.5;
     $('#figs').css('height', figs_height);
 
     $('.fig-image').css('top', fig_top);
@@ -31,17 +35,23 @@ jQuery(document).ready(function($) {
     $('#squiggle-2').css('top', squig2_top);
     $('#squiggle-3').css('top', squig3_top);
 
+    // What's In a Name? Layout
+    var name_top = ($('#name').offset()).top;
+
+    var name_height = $('#name').height();
+    console.log(name_height);
+
     //console.log(fig1_top);
 
     $(window).bind('scroll',function(e){
         parallaxScroll();
-        redrawDotNav();
     });
     function parallaxScroll() {
-        var scrolled = ($(window).scrollTop() - fig_top - 80);
+        var scrolled = ($(window).scrollTop() - fig_top - 200);
         if (scrolled <= 0) {scrolled = 0};
         if (scrolled > (figs_height - fig_txt_height)) {scrolled = (figs_height - fig_txt_height)};
-        $('#fig-text').css('top', (scrolled * .75) + 'px'); // Need to come up with test for this
+        $('#fig-text').css('top', Math.round(scrolled * .75) + 'px'); // Need to come up with test for this
+        console.log(Math.round(scrolled * .75));
         $('#fig-1').animate({top: (fig1_top - (scrolled * .3)) + 'px'}, 1);
         $('#fig-2').animate({top: (fig2_top - (scrolled * .4)) + 'px'}, 1);
         $('#fig-3').animate({top: (fig3_top - (scrolled * .25)) + 'px'}, 1);
